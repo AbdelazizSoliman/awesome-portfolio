dd94bb9e4bccc2b29ed6bf9e24518aed12369af7
Abdelaziz Soliman <abdelaziz.soliman89@gmail.com>
2024-10-09 13:53:26 +0300

    Create contact section

--- src/components/4-contact/Contact.jsx
+++ src/components/4-contact/Contact.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
 import './contact.css';
 
-export default function Contact() {
-  return (
-    <div>Contact</div>
-  );
-}
+const Contact = () => (
+  <section className="contact-us">
+    <h1 className="title">
+      <span className="icon-envelope"> </span>
+      Contact us
+    </h1>
+    <p className="sub-title">
+      Contact us for more information and Get notified when I publish something
+      new.
+    </p>
+
+    <div className="flex">
+      <form className="">
+        <div className="flex">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="email">Email Address:</label>
+          <input
+            autoComplete="off"
+            required
+            type="email"
+            name="email"
+            id="email"
+          />
+        </div>
+
+        <div className="flex" style={{ marginTop: '24px' }}>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="message">Your message:</label>
+          <textarea required name="message" id="message" />
+        </div>
+
+        <button type="button" className="submit">
+          Submit
+        </button>
+      </form>
+
+      <div className="border animation">animation</div>
+    </div>
+  </section>
+);
+
+export default Contact;
