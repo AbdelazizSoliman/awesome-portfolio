0ca3092e374afa47d51c945bb45cc64f57a5f9ce
Abdelrhman Ashrf <abdelrhmanashrf1997@gmail.com>
2024-10-10 13:36:29 +0300

    Footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import './footer.css';
 
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  );
+  return <>
+<footer className='d-flex justify-content-between mb-4'>
+  <ul className='d-flex gap-2 p-0'>
+    <li><a href="/">About</a></li>
+    <li><a href="/">Projects</a></li>
+    <li><a href="/">Speaking</a></li>
+    <li><a href="/">Uses</a></li>
+    <li></li>
+  </ul>
+  <p>© 2024 Spencer sharp. All rights reserved</p>
+</footer>
+  </>
 }
